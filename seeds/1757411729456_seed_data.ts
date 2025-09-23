f19141da616256b15fad4a2005c5e1b4f0c30bc1
zvukarci <149152199+zvukarci@users.noreply.github.com>
2025-09-23 12:35:23 +0200

    done

--- seeds/1757411729456_seed_data.ts
+++ seeds/1757411729456_seed_data.ts
@@ -1,31 +1,72 @@
 import { DB } from "@/lib/db-types";
-import type { Kysely } from "kysely";
 import { faker } from "@faker-js/faker";
+import type { Kysely } from "kysely";
 
 export async function seed(db: Kysely<DB>): Promise<void> {
-  await db.deleteFrom("albums").execute();
-  await db.deleteFrom("authors").execute();
+    await db.deleteFrom("songs").execute();
+    await db.deleteFrom("albums").execute();
+    await db.deleteFrom("authors").execute();
 
-  for (let i = 0; i < 10; i += 1) {
-    await db
-      .insertInto("authors")
-      .values({
-        name: faker.music.artist(),
-        bio: faker.lorem.paragraph(),
-      })
-      .execute();
-  }
+    for (let i = 0; i < 20; i += 1) {
+        const numBioParagraphs = faker.number.int({ min: 0, max: 5 });
 
-  const authors = await db.selectFrom("authors").selectAll().execute();
+        const bio =
+            numBioParagraphs !== 0
+                ? faker.lorem.paragraph(numBioParagraphs)
+                : null;
 
-  for (const author of authors) {
-    await db
-      .insertInto("albums")
-      .values({
-        author_id: author.id,
-        name: faker.music.album(),
-        release_date: faker.date.past().getTime(),
-      })
-      .execute();
-  }
+        await db
+            .insertInto("authors")
+            .values({
+                name: faker.music.artist(),
+                bio,
+            })
+            .execute();
+    }
+
+    const authors = await db.selectFrom("authors").selectAll().execute();
+
+    for (const author of authors) {
+        const numAlbums = faker.number.int({ min: 0, max: 10 });
+
+        for (let i = 0; i < numAlbums; i += 1) {
+            await db
+                .insertInto("albums")
+                .values({
+                    author_id: author.id,
+                    name: faker.music.album(),
+                    release_date: faker.date.past().getTime(),
+                })
+                .execute();
+        }
+    }
+
+    const albums = await db.selectFrom("albums").selectAll().execute();
+
+    for (const album of albums) {
+        const typeOfAlbum = faker.number.int({ min: 0, max: 9 });
+
+        let numSongs = 1;
+
+        if (typeOfAlbum < 2) {
+            numSongs = 1;
+        } else if (typeOfAlbum < 5) {
+            numSongs = faker.number.int({ min: 4, max: 6 });
+        } else {
+            numSongs = faker.number.int({ min: 10, max: 20 });
+        }
+
+        console.log(album.name, numSongs);
+
+        for (let i = 0; i < numSongs; i += 1) {
+            await db
+                .insertInto("songs")
+                .values({
+                    album_id: album.id,
+                    name: faker.music.songName(),
+                    duration: faker.number.int({ min: 90, max: 240 }),
+                })
+                .execute();
+        }
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,56 @@
 import Image from "next/image";
+import SQLite from "better-sqlite3";
+import { Kysely, SqliteDialect } from "kysely";
+import { DB } from "@/lib/db-types";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p className="text-4xl font-bold">Spotify</p>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>Footer</p>
-      </footer>
-    </div>
-  );
+export default async function Home() {
+    const dialect = new SqliteDialect({ database: new SQLite("db.sqlite") });
+    const db = new Kysely<DB>({ dialect });
+
+    const albums = await db
+        .selectFrom("albums")
+        .innerJoin("authors", "authors.id", "author_id")
+        .select([
+            "albums.id",
+            "albums.name",
+            "albums.release_date",
+            "authors.name as author_id",
+        ])
+        .execute();
+
+    console.log(albums[0]);
+
+    return (
+        <main className="bg-black min-h-screen p-8">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-center mb-12">
+                    <Image
+                        src="/spotify.png"
+                        width={40}
+                        height={40}
+                        alt="Logo"
+                        className="hover:scale-110 transition-transform"
+                    />
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {albums.map((album) => (
+                        <div
+                            key={album.id}
+                            className="bg-white/10 p-6 rounded-lg hover:bg-white/15 transition-colors"
+                        >
+                            <h2 className="text-xl font-bold mb-2 text-white">
+                                {album.author_id}
+                            </h2>
+                            <h3 className="text-lg text-white/80 mb-4">
+                                {album.name}
+                            </h3>
+                            <span className="text-sm text-white/50 font-semibold">
+                                {new Date(album.release_date).toDateString()}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    );
 }
